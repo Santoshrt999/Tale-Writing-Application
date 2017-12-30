82a68bc36eee9fbde55770fe0dc70cc4711eecbf
Santoshrt999 <gsr.teja@gmail.com>
2017-12-30 09:48:42 -0600

    Initial web devloping started

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express =require('express');
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+
 //load model
 
 require('./models/User');
@@ -10,6 +15,7 @@ require('./config/passport')(passport);
 
 const app=express(); //adding express to the app
 //load routes from the folder..
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 //load keys
 const keys = require('./config/keys');
@@ -25,16 +31,36 @@ useMongoClient: true
 .then(()=>{
     console.log('MongDB Database Connected');
 }).catch(err => console.log(err));
+//handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
+//express-session 
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
 
+//passport midleware
+app.use(passport.initialize());
+app.use(passport.session());
 
+//global vars
+app.use((req,res,next)=>{
+    res.locals.user = req.user || null;
+    next();
+})
 
-app.get('/', (req,res)=>{
-    res.send('Hi');
-});
 
 //use route and connnecting it
+app.use('/', index);
 app.use('/auth', auth);
+
+
 
 const port = process.env.PORT ||5000;
 app.listen(port, ()=>{
--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,50 @@
-const GoogleStrategy = require('passport-google-oauth20' ).Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
+const User = mongoose.model('users');
+
 module.exports = function(passport){
-    passport.use(
-        new GoogleStrategy({
-            clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
-            },(accessToken, refreshToken, profile, done)=>{
-              //code to do..
-console.log(profile);
+  passport.use(
+    new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret:keys.googleClientSecret,
+      callbackURL:'/auth/google/callback',
+      proxy: true
+    }, (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(profile);
+
+      const image = profile.photos[0].value.substring(0,
+    profile.photos[0].value.indexOf('?'));
+    const newUser = {
+        googleID: profile.id,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        email: profile.emails[0].value,
+        image: image
+    }
+    User.findOne({
+        googleID: profile.id
+    }).then(user=>{
+        if(user){   //if user we return null and that user
+        done(null,user);
+        }else{
+            //else we create user and return null
+            new User(newUser)
+            .save()
+            .then(user=>{
+                done(null,user);
             })
-    )
+        }
+    })
+    })
+  );
+  passport.serializeUser((user,done)=>{
+      done(null, user.id);
+  });
+  passport.deserializeUser((id,done)=>{   //id for deserializing the elements
+User.findById(id).then(user=> done(null, user));
+  });
 
 }
