4ef1534dbfe97694fa8f9cb5f62f8b255383fbcc
Santoshrt999 <gsr.teja@gmail.com>
2017-12-29 20:17:03 -0600

    changed the port for error refrence in heroku

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/', (req,res)=>{
 //use route and connnecting it
 app.use('/auth', auth);
 
-const port =5000;
+const port = process.env.PORT ||5000;
 app.listen(port, ()=>{
     console.log("App is listening");
 });
